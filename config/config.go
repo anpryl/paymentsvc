5e50a73b72b6fb5c2576019d1aa234fd93995c3e
Anatolii Prylutskyi <anpryl@gmail.com>
2019-02-17 16:33:27 +0200

    temporary remove unused config values

--- config/config.go
+++ config/config.go
@@ -7,15 +7,15 @@ import (
 )
 
 type Config struct {
-	PostgreSQL struct {
-		Host     string
-		Port     int
-		Database struct {
-			User     string
-			Password string
-			Name     string
-		}
-	}
+	// PostgreSQL struct {
+	// Host     string
+	// Port     int
+	// Database struct {
+	// User     string
+	// Password string
+	// Name     string
+	// }
+	// }
 	Http struct {
 		Host string `envconfig:"default=127.0.0.1"`
 		Port int    `envconfig:"default=9090"`
