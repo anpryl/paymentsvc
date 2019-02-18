07dfdb31aadd132773ca6a80091021986b2191cd
Anatolii Prylutskyi <anpryl@gmail.com>
2019-02-18 14:24:01 +0200

    Add scripts for integration_tests

---
+++ scripts/integration_tests.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env sh
+
+docker-compose -f docker-compose-integration-tests.yml up -d db paymentsvc
+docker-compose -f docker-compose-integration-tests.yml up --exit-code-from test test
+test_result=$?
+docker-compose -f docker-compose-integration-tests.yml logs
+docker-compose -f docker-compose-integration-tests.yml down --rmi local
+exit $test_result
