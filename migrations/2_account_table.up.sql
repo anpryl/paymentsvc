03a967ce33bdf4e77ebe07ef8c428ffbaeb27094
Anatolii Prylutskyi <anpryl@gmail.com>
2019-02-19 01:04:57 +0200

    Add check for non-empty balance

--- migrations/2_account_table.up.sql
+++ migrations/2_account_table.up.sql
@@ -1,5 +1,7 @@
 CREATE TABLE accounts (
-       id uuid DEFAULT uuid_generate_v4() NOT NULL,
+       id uuid DEFAULT uuid_generate_v4() PRIMARY KEY,
        currency_numeric_code smallint NOT NULL,
-       balance numeric NOT NULL
+       balance numeric NOT NULL,
+
+       CONSTRAINT accounts_nonnegative_balance CHECK (balance >= 0)
 );
