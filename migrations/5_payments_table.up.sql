9ad1c678dc20ee0b2aaa99f00808ee36c44b9c03
Anatolii Prylutskyi <anpryl@gmail.com>
2019-02-19 01:05:35 +0200

    Add payments history table

---
+++ migrations/5_payments_table.up.sql
@@ -0,0 +1,23 @@
+CREATE TABLE payments (
+       id uuid DEFAULT uuid_generate_v4() PRIMARY KEY,
+       from_account uuid NOT NULL,
+       to_account uuid NOT NULL,
+       currency_numeric_code smallint NOT NULL,
+       amount numeric NOT NULL,
+       created_at time NOT NULL,
+
+       CONSTRAINT fk_from_account
+       FOREIGN KEY (from_account)
+       REFERENCES accounts(id)
+       ON DELETE RESTRICT
+       ON UPDATE CASCADE,
+
+       CONSTRAINT fk_to_account
+       FOREIGN KEY (to_account)
+       REFERENCES accounts(id)
+       ON DELETE RESTRICT
+       ON UPDATE CASCADE
+);
+
+CREATE INDEX payments_from_account_idx 
+    ON payments(from_account);
