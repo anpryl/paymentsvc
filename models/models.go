2e80a2110ba39b019ff853472adc04f2978ebe39
Anatolii Prylutskyi <anpryl@gmail.com>
2019-02-18 14:23:05 +0200

    Remove CurrencyCode type, add new to models

--- models/models.go
+++ models/models.go
@@ -2,27 +2,29 @@ package models
 
 import uuid "github.com/satori/go.uuid"
 
-type CurrencyCode string
-
-const (
-	CurrencyCodeUSD CurrencyCode = "USD"
-	CurrencyCodeEUR CurrencyCode = "EUR"
-	CurrencyCodeUAH CurrencyCode = "UAH"
-	CurrencyCodeBYN CurrencyCode = "BYN"
-	CurrencyCodeRUB CurrencyCode = "RUB"
-)
-
 // Currency - basic info about currency
 type Currency struct {
-	Code  CurrencyCode
-	Minor uint // Number of decimal units
+	AlphaCode   string
+	NumericCode int
+	Minor       uint // Number of decimal units
 }
 
 // Account - information about account
 type Account struct {
-	ID           uuid.UUID
-	CurrencyCode CurrencyCode
+	ID                   uuid.UUID `json:"id"`
+	CurrencyNumbericCode int       `json:"currency_numberic_code"`
 
 	// Balance is stored in minimal units. For example for USD it would be in cents.
-	Balance uint64
+	Balance uint64 `json:"balance"`
+}
+
+const (
+	DefaultOffset = 0
+	DefaultLimit  = 100
+)
+
+// OffsetLimit - used for database access
+type OffsetLimit struct {
+	Offset int
+	Limit  int
 }
