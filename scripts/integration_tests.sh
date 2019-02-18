79c1ff91f7529e5808ae3a6cdca76c31e2f0c8be
Anatolii Prylutskyi <anpryl@gmail.com>
2019-02-18 14:26:05 +0200

    integration_tests script show only paymentsvc logs

--- scripts/integration_tests.sh
+++ scripts/integration_tests.sh
@@ -3,6 +3,6 @@
 docker-compose -f docker-compose-integration-tests.yml up -d db paymentsvc
 docker-compose -f docker-compose-integration-tests.yml up --exit-code-from test test
 test_result=$?
-docker-compose -f docker-compose-integration-tests.yml logs
+docker-compose -f docker-compose-integration-tests.yml logs paymentsvc
 docker-compose -f docker-compose-integration-tests.yml down --rmi local
 exit $test_result
