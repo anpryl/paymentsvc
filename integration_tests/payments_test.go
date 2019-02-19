b9cff70d792bb7e51eb0de5adbaad9e38582efbd
Anatolii Prylutskyi <anpryl@gmail.com>
2019-02-19 04:23:21 +0200

    Add concurrenct payment creation red test

--- integration_tests/payments_test.go
+++ integration_tests/payments_test.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"net/http"
+	"sync"
 	"testing"
 
 	"github.com/anpryl/paymentsvc/api"
@@ -60,6 +61,64 @@ func TestCreatePayment(t *testing.T) {
 	_, err = httpGetJSON("/accounts/"+idResp2.ID.String(), &acc2)
 	a.EqualValues(idResp2.ID, acc2.ID)
 	a.EqualValues(decimal.NewFromFloat(10950.56).String(), acc2.Balance.String())
+	a.Nil(err)
+}
+
+func TestConcurrentCreatePayment(t *testing.T) {
+	a := assert.New(t)
+	uah := 980
+	acc := services.NewAccount{
+		CurrencyNumericCode: uah,
+		Balance:             decimal.NewFromFloat(1000),
+	}
+	var idResp1 api.IDResp
+	resp, err := httpPostJSON("/accounts", acc, &idResp1)
+	if a.Nil(err) {
+		a.NotZero(idResp1)
+		a.Equal(http.StatusCreated, resp.StatusCode)
+	}
+	var idResp2 api.IDResp
+	resp, err = httpPostJSON("/accounts", acc, &idResp2)
+	if a.Nil(err) {
+		a.NotZero(idResp2)
+		a.Equal(http.StatusCreated, resp.StatusCode)
+	}
+
+	payment := services.NewPayment{
+		FromAccount:         idResp1.ID,
+		ToAccount:           idResp2.ID,
+		CurrencyNumericCode: uah,
+		Amount:              decimal.NewFromFloat(950.556),
+	}
+	wait := make(chan struct{})
+	var wg sync.WaitGroup
+	for i := 0; i < 20; i++ {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			<-wait
+			var paymentID api.IDResp
+			_, _ = httpPostJSON("/payments", payment, &paymentID)
+		}()
+	}
+	close(wait)
+	wg.Wait()
+
+	var ps []models.Payment
+	_, err = httpGetJSON("/accounts/"+payment.FromAccount.String()+"/payments", &ps)
+	a.Nil(err)
+	a.Len(ps, 1)
+
+	var acc1 models.Account
+	_, err = httpGetJSON("/accounts/"+idResp1.ID.String(), &acc1)
+	a.Nil(err)
+	a.EqualValues(idResp1.ID, acc1.ID)
+	a.EqualValues(decimal.NewFromFloat(049.44).String(), acc1.Balance.String())
+
+	var acc2 models.Account
+	_, err = httpGetJSON("/accounts/"+idResp2.ID.String(), &acc2)
+	a.EqualValues(idResp2.ID, acc2.ID)
+	a.EqualValues(decimal.NewFromFloat(1950.56).String(), acc2.Balance.String())
 	a.Nil(err)
 }
 
